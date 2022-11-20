d31af035f0e033eefae5fb995327517d3e814333
krzysztofMlczk <krzysztof.malczak@ardentcode.com>
2022-11-20 11:07:20 +0100

    improve comments for subscription schema

--- database/schemas/subscription.schema.js
+++ database/schemas/subscription.schema.js
@@ -3,8 +3,14 @@ const mongoose = require("mongoose");
 const subscriptionSchema = mongoose.Schema({
   endpoint: String,
   expirationTime: Object, // usually null
+  // Eliptic-curve Diffie Hellman keys for push message encryption - see: https://datatracker.ietf.org/doc/html/draft-ietf-webpush-encryption
   keys: {
+    // public key
+    // NOTE: when browser creates PushSubscription it saves
+    // corresponding private key, so it can decrypt the
+    // message when it arrives from the (potentially insecure) PushService
     p256dh: String,
+    // shared secret
     auth: String,
   },
 });
